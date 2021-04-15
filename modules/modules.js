1b8ac8f4b54f34c2d0f96b99f9eaec513369b6a9
Averagess <45748163+Averagess@users.noreply.github.com>
2021-04-16 00:41:25 +0300

    +number converter function

--- modules/modules.js
+++ modules/modules.js
@@ -4,4 +4,7 @@ module.exports = {
 		const time = moment().format("LTS");
 		return `[${time}]`;
 	},
+	readableNumber: function(number) {
+		return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+	},
 };
