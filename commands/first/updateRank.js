2eb3485616799ed92f541ca9f08d2533fb638594
Averagess <45748163+Averagess@users.noreply.github.com>
2021-04-26 20:44:38 +0300

    small description change

--- commands/first/updateRank.js
+++ commands/first/updateRank.js
@@ -14,7 +14,7 @@ module.exports = class updaterank extends Command {
 		const embed = new MessageEmbed()
 			.setColor("#FFA500")
 			.setTitle("Bot's Steam profile link")
-			.setDescription("In order for the bot to know your rank, you need to add it to your friends list on steam. We wont message you on steam, and we will remove you almost instantly so we wont take any friends list space.")
+			.setDescription("In order for the bot to know your rank, you need to add it to your steam friends list. We wont message you on steam, and we will remove you almost instantly so we wont take any friends list space.")
 			.setURL("https://steamcommunity.com/id/ricksawBot")
 			.setTimestamp()
 			.setFooter("Ricksaw CSGO Bot", this.client.user.displayAvatarURL());
