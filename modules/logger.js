4e5556761f31f12b307c369dda50f0c9f0871f93
Averagess <45748163+Averagess@users.noreply.github.com>
2021-05-04 15:59:58 +0300

    logger export

---
+++ modules/logger.js
@@ -0,0 +1,30 @@
+const winston = require("winston");
+const { combine, timestamp, printf, prettyPrint, metadata, colorize } = winston.format;
+
+
+const timezoned = () => {
+	return new Date().toLocaleString("en-GB", {
+		timeZone: "Europe/Helsinki",
+	});
+};
+
+const logFormat = printf(info => `[${info.timestamp}] [${info.level}] ${info.message}`);
+const logger = winston.createLogger({
+	format: combine(
+		timestamp({ format : timezoned }),
+		prettyPrint(),
+		metadata({ fillExcept: ["message", "level", "timestamp"] }),
+	),
+	transports: [
+		new winston.transports.Console({
+			format: combine(
+				colorize(),
+				logFormat),
+			handleRejections: true,
+			// handleExceptions: true,
+		}),
+		new winston.transports.File({ filename: "../discord-bot.log", format: winston.format.json(), handleRejections: true, handleExceptions: true }),
+	],
+});
+
+module.exports = logger;
