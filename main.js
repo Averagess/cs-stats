ad5502437b111f0b07f907f77e21b539c394c0e5
Averagess <45748163+Averagess@users.noreply.github.com>
2021-05-10 22:52:09 +0300

    Update main.js

--- main.js
+++ main.js
@@ -71,6 +71,9 @@ client.on("commandRun", (command, promise, message, args, fromPattern, result) =
 	if (message.guild == null && result != null) {
 		return logger.info(`[cmdran] ${message.author.tag} ran cmd [${command.name}] with args [${result.values[Object.keys(result.values)[0]]}] in DM's`);
 	}
+	if (message.guild == null && result == null) {
+		return logger.info(`[cmdran] ${message.author.tag} ran cmd [${command.name}] in DM's`);
+	}
 	// If we have args and msg was in a guild, log this
 	logger.info(`[cmdran] ${message.author.tag} ran cmd [${command.name}] with args [${result.values[Object.keys(result.values)[0]]}] in server [${message.guild.name}]`);
 });
