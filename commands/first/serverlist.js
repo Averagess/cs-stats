5ce1fc2ec7ec146db1536bde85038a6580281c7c
Averagess <45748163+Averagess@users.noreply.github.com>
2021-10-24 14:02:21 +0300

    replaced id to tag

--- commands/first/serverlist.js
+++ commands/first/serverlist.js
@@ -1,6 +1,6 @@
 const { Command } = require("discord.js-commando");
 const { MessageEmbed } = require("discord.js");
-
+const { time } = require("../../modules/modules");
 module.exports = class MeowCommand extends Command {
 	constructor(client) {
 		super(client, {
@@ -23,7 +23,7 @@ module.exports = class MeowCommand extends Command {
 		const embed = new MessageEmbed();
 		this.client.guilds.cache.forEach(i => {
 			count++;
-			embed.addField(`${count}. ${i.name}`, `Members: ${i.memberCount}\nLarge: ${i.large}\nOwner: ${i.ownerID}\nBoosts: ${i.premiumSubscriptionCount}\nVerified: ${i.verified}\nJoined at: ${i.joinedAt.toLocaleString()}`);
+			embed.addField(`${count}. ${i.name}`, `Members: ${i.memberCount}\nLarge: ${i.large}\nOwner: ${i.owner}\nBoosts: ${i.premiumSubscriptionCount}\nVerified: ${i.verified}\nJoined at: ${i.joinedAt.toLocaleString()}`);
 		});
 		return message.say(embed);
 	}
